fae59b773a263a9473db8faa530d26f85f94d660
andrewgwatson <andrew@andrewgwatson.com>
2021-05-18 22:37:53 +0100

    erm ...

--- graceland/climate.py
+++ graceland/climate.py
@@ -23,36 +23,51 @@ except ImportError:
     from stubs import grow_STUB
     GROW = grow_STUB
  
+def snapshot():
+    ss = dict(
+        temp = current_air_temperature(),
+        humidity = current_humidity(),
+        airpressure = current_air_pressure(),
+        light = current_light_level(),
+        moisturesensors = [current_soil_moisture(1),current_soil_moisture(3),current_soil_moisture(3)]
+        ) 
+    return ss
 
 def current_air_temperature():
         t = BME280.get_temperature()
-        logging.info("""Temp: {:06.03f} {:}""".format(t, config.units['temp']))
+        t = f'{t:.3f}'
+        logging.info("""Temp: {:} {:}""".format(t, config.units['temp']))
         return t
 
 def current_humidity():
         h = BME280.get_humidity()
-        logging.info("""Humidity: {:06.03f} {:}""".format(h, config.units['humidity']))
+        h = f'{h:.2f}'
+        logging.info("""Humidity: {:} {:}""".format(h, config.units['humidity']))
         return h
 
 def current_air_pressure():
         p = BME280.get_pressure()
-        logging.info("""Pressure: {:06.03f} {:}""".format(p, config.units['airpressure']))
+        p = f'{p:.2f}'
+        logging.info("""Pressure: {:} {:}""".format(p, config.units['airpressure']))
         return p
         
 def current_light_level():
         l = LTR559.get_lux()
-        logging.info("""Light: {:06.03f} {:}""".format(l, config.units['light']))
+        l = f'{l:.0f}'
+        logging.info("""Light: {:} {:}""".format(l, config.units['light']))
         return l
 #       prox = ltr559.get_proximity()
         
 def current_soil_moisture(sensor):
         m = GROW.moisture()
-        logging.info("""Soil Moisture on sensor {:} : {:06.03f} {:}""".format(sensor, m, config.units['moisture']))
+        m = f'{m:.2f}'
+        logging.info("""Soil Moisture on sensor {:} : {:} {:}""".format(sensor, m, config.units['moisture']))
         return m
 
-#logging.info("""Light: {:05.02f} Lux""".format(current_light_level()))
+
 
 def headless():
+        print(snapshot())
         print('Number of arguments:', len(sys.argv), 'arguments.')
         print('Argument List:', str(sys.argv))
 
--- graceland/core.py
+++ graceland/core.py
@@ -19,21 +19,22 @@ import config, climate
 
 def main():
     
-    # logging.basicConfig(filename='graceland.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s.%(msecs)03d %(levelname)-2s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s.%(msecs)03d %(levelname)-2s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(filename='graceland.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s.%(msecs)03d %(levelname)-2s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s.%(msecs)03d %(levelname)-2s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-    c = climate
-    logging.info("""Core Execution Started""")
-    c.current_air_temperature()
-    c.current_humidity()
-    c.current_air_pressure()
-    c.current_light_level()
-    c.current_soil_moisture(1)
-    print(config.units["temp"])
-    
+    c = climate.snapshot()
     # Write sensor data to csv as a simple timeseries
     with open(config.db["filename"], 'a', newline='') as csvfile:
         db = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        db.writerow([f"{datetime.now():%Y-%m-%d %H:%M:%S}",1,2,3,4,5,6,7])
+        # db.writerow([f"{datetime.now():%Y-%m-%d %H:%M:%S}",c])
+        db.writerow([f"{datetime.now():%Y-%m-%d %H:%M:%S}",
+                     c['temp'],
+                     c['humidity'],
+                     c['airpressure'],
+                     c['light'],
+                     c['moisturesensors'][0],
+                     c['moisturesensors'][1],
+                     c['moisturesensors'][2],
+                     ])
         
 if __name__ == "__main__" : main()
